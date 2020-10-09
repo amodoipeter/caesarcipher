3be3e1f4b65d6884b776d479665af0f56ff033f0
amodoipeter <amodoipeter@gmail.com>
2020-10-10 00:45:54 +0300

    Declared variables

---
+++ CaesarCipher.java
@@ -0,0 +1,14 @@
+import  java.util.scanner;
+
+public class CaesarCipher{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String word = " ", pText = " ", newWord = "";
+        System.out.println("Enter the string you would like to encrypt: ");
+        pText = input.nextLine();
+        word = cipher(pText);
+        newWord = deCipher(word);
+
+        System.out.println("Plain text: %s%n" + "Cipher Text: %s%n" + "Plain text: %s%n%n", pText, word, newWord);
+    }
+}
